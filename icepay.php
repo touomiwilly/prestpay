9f5a1fac8ab2c3ee466386f982f2ca5d2f53edd6
Ricardo Jacobs <ricardozegt@gmail.com>
2015-09-21 10:17:53 +0200

    Increasing version numbers.

--- icepay.php
+++ icepay.php
@@ -4,7 +4,7 @@
  * @package       ICEPAY Payment Module for Prestashop
  * @author        Ricardo Jacobs <ricardo.jacobs@icepay.com>
  * @copyright     (c) 2015 ICEPAY. All rights reserved.
- * @version       2.1.0, August 2015
+ * @version       2.1.1, September 2015
  * @license       BSD 2 License, see https://github.com/icepay/Prestashop/blob/master/LICENSE.md
  */
 
